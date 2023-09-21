a1c88b93ce4366439ebc052cb4477cfa816b58de
fixoss <fixoss@fastmail.com>
2023-09-21 19:15:23 +0100

    updated with classes thanks to the neighbour

--- pizza.py
+++ pizza.py
@@ -1,40 +1,58 @@
 import json
 
-def run():
-	recipe_dict = {}
-
+class PizzaRecipe:
 	recipe_path = "./recipe.json"
 
-	try:
-		with open(recipe_path, "r") as f:
+	def __init__(self, num_pizzas=4, ball_weight_g=240, hydration=60):
+		self.num_pizzas = num_pizzas
+		self.ball_weight_g = ball_weight_g
+		self.hydration = hydration
+	
+	def save(self, path=recipe_path):
+		recipe_dict = {
+			"num_pizzas": self.num_pizzas,
+			"ball_weight_g": self.ball_weight_g,
+			"hydration": self.hydration
+		}
+		with open(path, 'w') as f:
+			json.dump(recipe_dict, f, ensure_ascii=True)
+
+	def load(self, path=recipe_path):
+		with open(path, 'r') as f:
 			recipe_dict = json.loads(f.read())
-	except Exception as e:
-		#print(e)
-		pass
 
-	recipe_dict["num_pizzas"] = recipe_dict.get("num_pizzas", 4)
+
+def run():
+	# Let there be pizza
+	mypizza = PizzaRecipe()
+
 	try:
-		recipe_dict["num_pizzas"] = int(input(f"Number of pizzas ({recipe_dict['num_pizzas']}): "))
+		# Try to grab the previous pizza recipe
+		mypizza.load()
+	except IOError as e:
+		# If no recipes exist, then we start a new recipe from scratch
+		mypizza = PizzaRecipe()
+
+	try:
+		mypizza.num_pizzas = int(input(f"Number of pizzas ({mypizza.num_pizzas}): "))
 	except:
 		pass
 
-	recipe_dict["ball_weight_g"] = recipe_dict.get("ball_weight_g", 240)
 	try:
-		recipe_dict["ball_weight_g"] = int(input(f"Size grams ({recipe_dict['ball_weight_g']}g): "))
+		mypizza.ball_weight_g = int(input(f"Size grams ({mypizza.ball_weight_g}g): "))
 	except:
 		pass
 
-	recipe_dict["hydration"] = recipe_dict.get("hydration", 60)
 	try:
-		recipe_dict["hydration"] = int(input(f"Hydration ({recipe_dict['hydration']}%): "))
+		mypizza.hydration = int(input(f"Hydration ({mypizza.hydration}%): "))
 	except:
 		pass
 
-	dough_weight_g = recipe_dict["num_pizzas"] * recipe_dict["ball_weight_g"]
+	dough_weight_g = mypizza.num_pizzas * mypizza.ball_weight_g
 	flour_ratio = 100
-	water_ratio = recipe_dict["hydration"]
+	water_ratio = mypizza.hydration
 	salt_ratio = 3
-	yeast_ratio = 0.15
+	yeast_ratio = 0.08
 
 	total_ratio = flour_ratio + water_ratio + yeast_ratio + salt_ratio
 
@@ -44,19 +62,14 @@ def run():
 	salt_weight_g = dough_ratio * salt_ratio
 	yeast_weight_g = dough_ratio * yeast_ratio
 
-	yeast_tsps_to_g = 1.0 / 2.8
-	yeast_tsps = yeast_weight_g * yeast_tsps_to_g
-
 	results = ["---"]
 	results += [f"Flour ({flour_ratio}%): {flour_weight_g:.0f}g"]
 	results += [f"Water ({water_ratio}%): {water_weight_g:.0f}g"]
 	results += [f"Salt ({salt_ratio}%): {salt_weight_g:.2f}g"]
-	results += [f"Yeast ({yeast_ratio}%): {yeast_weight_g:.2f}g or {yeast_tsps:.2f} tsps"]
+	results += [f"Yeast ({yeast_ratio}%): {yeast_weight_g:.2f}g"]
 
+	mypizza.save()
 	print("\n".join(results))
-
-	with open(recipe_path, "w") as f:
-		json.dump(recipe_dict, f, ensure_ascii=True)
 
 if __name__ == "__main__":
 	run()
